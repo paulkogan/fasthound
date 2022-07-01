e42de81dc8d1e20197f674fa15953313d6b7d3c9
Paul Kogan <paulkog@gmail.com>
2022-07-01 07:50:06 -0400

    update requirements

--- core/energizers.py
+++ core/energizers.py
@@ -7,59 +7,9 @@ from sqlalchemy import select, update
 from sqlalchemy.orm import Session
 from models import Energizer
 
-# from core.test_messages.serializers import TestMessageRequest
-
 
 def list_energizers(
     session_factory: Callable[[], ContextManager[Session]]
 ) -> list[Energizer]:
     with session_factory() as session:
         return list(session.execute(select(Energizer)).scalars())
-
-
-# def list_test_messages(session_factory: Callable[[], ContextManager[Session]]) -> list[TestMessage]:
-#     with session_factory() as session:
-#         return list(session.execute(select(TestMessage)).scalars())
-
-
-# def get_test_message(message_id: UUID, session_factory: Callable[[], ContextManager[Session]]) -> TestMessage:
-#     with session_factory() as session:
-#         return session.execute(select(TestMessage).where(TestMessage.id == message_id)).scalar_one()
-
-
-# def update_test_message(
-#     message_id: UUID, payload: TestMessageRequest, session_factory: Callable[[], ContextManager[Session]]
-# ) -> TestMessage:
-#     with session_factory() as session:
-#         stmt = (
-#             update(TestMessage)
-#             .where(TestMessage.id == message_id)
-#             .values(
-#                 prescriber_id=payload.prescriber_id,
-#                 drug_id=payload.drug_id,
-#                 status=payload.status,
-#                 prescriber_order_number=payload.prescriber_order_number,
-#             )
-#             .returning(TestMessage)
-#         )
-#         orm_stmt = select(TestMessage).from_statement(stmt).execution_options(populate_existing=True)
-#         test_message = session.execute(orm_stmt).scalar_one()
-#         session.commit()
-#         return test_message
-# def create_test_message(
-#     payload: TestMessageRequest, session_factory: Callable[[], ContextManager[Session]]
-# ) -> TestMessage:
-#     test_message = TestMessage(
-#         id=uuid4(),
-#         prescriber_id=payload.prescriber_id,
-#         drug_id=payload.drug_id,
-#         status=payload.status,
-#         prescriber_order_number=payload.prescriber_order_number,
-#         created=datetime.now(tz=pytz.UTC),
-#     )
-
-#     with session_factory() as session:
-#         session.add(test_message)
-#         session.commit()
-
-#     return test_message
